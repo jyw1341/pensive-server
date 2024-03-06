cd73d39fd8b7a5edf783f5c342685be60e46cc1a
정용우 <jyw1341@gmail.com>
2024-03-07 03:45:13 +0900

    first commit

---
+++ src/main/java/com/pensive/PensiveApplication.java
@@ -0,0 +1,13 @@
+package com.pensive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PensiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pensive/PensiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pensive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
